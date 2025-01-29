a2f1a81da3d1a136a5d54d267a81d1185f613958
Patric Gekoski <=>
2025-01-29 11:56:03 -0500

    Add tanstack/react-query implementation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,30 @@
-import { Stack } from "expo-router";
+import { useReactQueryDevTools } from '@dev-plugins/react-query/build/useReactQueryDevTools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Stack } from 'expo-router'
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			staleTime: 1000 * 60,
+		},
+	},
+})
 
 export default function RootLayout() {
-  return <Stack />;
+	useReactQueryDevTools(queryClient)
+
+	return (
+		<QueryClientProvider client={queryClient}>
+			<Stack>
+				<Stack.Screen
+					name='index'
+					options={{
+						title: 'Galactic Products',
+						headerShadowVisible: false,
+					}}
+				/>
+			</Stack>
+		</QueryClientProvider>
+	)
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,24 @@
+import { useQuery } from '@tanstack/react-query'
 import { Text, View } from 'react-native'
+import { getCategories, getProducts } from './utils/api'
 
 export default function Index() {
+	const { data: products } = useQuery({
+		queryKey: ['products'],
+		queryFn: getProducts,
+	})
+
+	console.log(products)
+
+	const { data: categories = [] } = useQuery({
+		queryKey: ['categories'],
+		queryFn: getCategories,
+	})
+
+	console.log(categories)
+
+	const allCategories = ['all', ...categories]
+
 	return (
 		<View
 			style={{
